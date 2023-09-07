1598b7080e5b8c1275c9b3e4ac7d629d1565a479
David Choloyan <david.choloyan@merkatos.cz>
2023-09-07 12:09:24 +0200

    fix: translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next-i18next').UserConfig} */
+
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
+    localePath: path.resolve("./public/locales"),
+    localeStructure: "{{lng}}/{{ns}}",
     locales: ["cs", "en"],
     localeDetection: false,
   },
